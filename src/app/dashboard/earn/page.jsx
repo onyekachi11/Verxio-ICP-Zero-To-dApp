c984cfa0052e9a505c7dd36d99b170c5873ccba2
onyekachii <emegwaonyeka@gmail.com>
2024-02-06 17:52:08 +0100

    checking earn page error 3

--- src/app/dashboard/earn/page.jsx
+++ src/app/dashboard/earn/page.jsx
@@ -3,12 +3,12 @@
 import JobCard from "../../../components/jobComponent/JobCard";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-// import { listDocs } from "@junobuild/core";
-// import { authSubscribe } from "@junobuild/core";
+import { listDocs } from "@junobuild/core";
+import { authSubscribe } from "@junobuild/core";
 
-// import { AuthContext } from "../../../components/auth";
-// import { useNav } from "../../../context/nav_context";
-// import { root } from "../../../../store";
+import { AuthContext } from "../../../components/auth";
+import { useNav } from "../../../context/nav_context";
+import { root } from "../../../../store";
 
 const Page = () => {
   const [jobs, setJobs] = useState([]);
@@ -17,39 +17,39 @@ const Page = () => {
   const userProfile = useSelector((state)=> state.persistedReducer.user.userProfile)
 
 
-  // const list = async () => {
-  //   try {
-  //     const { items } = await listDocs({
-  //       collection: "publish-task",
-  //     });
-  //     setJobs(items);
+  const list = async () => {
+    try {
+      const { items } = await listDocs({
+        collection: "publish-task",
+      });
+      setJobs(items);
 
-  //   } catch (error) {
-  //     console.error("Error fetching data:", error);
-  //   }
-  // };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-  // // list()
+  // list()
 
-  // console.log(user)
-  // console.log(userProfile)
+  console.log(user)
+  console.log(userProfile)
 
-  // useEffect(() => {
-  //   if (user) {
-  //     list()
-  //   }
-  // }, [user]);
+  useEffect(() => {
+    if (user) {
+      list()
+    }
+  }, [user]);
 
   return (
     <>
       <div className="border p-[32px] rounded-2xl">
         <h2 className="text-primary text-[28px] font-semibold mb-[32px] capitalize">
-          {/* Welcome back {userProfile.firstName} */}
-          hello
+          Welcome back {userProfile.firstName}
+          {/* hello */}
         </h2>
-        {/* {jobs?.map((item) => (
+        {jobs?.map((item) => (
           <JobCard key={item.key} jobs={item} />
-        ))} */}
+        ))}
       </div>
     </>
   );
