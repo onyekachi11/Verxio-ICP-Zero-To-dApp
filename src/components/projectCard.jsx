114412a093f7d2d5dfb97bb28e03004eed230cb6
amdonatusprince <donatusprince@gmail.com>
2024-02-09 13:37:50 +0100

    project page updated

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -4,17 +4,17 @@ import chat from "../assets/comment-white.svg"
 
 
 const ProjectCard = ({ item }) => {
-  const { label, time, desc, name } = item;
+  const { applicantFirstName, description, title, applicantlastName} = item;
   return (
     <>
       <div className="border bg-[#F7F7FD] rounded-2xl px-[32px] py-[28px] ">
         <div className="flex justify-between mb-[24px] ">
           <div>
             <p className="text-[20px] font-bold mb-2">
-              {label}
+              {title}
             </p>
             <p className="text-[16px] font-medium">
-              This agreement was entered into on [{time}]
+              This agreement is with {applicantFirstName} {applicantlastName}
             </p>
           </div>
           <Button
@@ -22,9 +22,9 @@ const ProjectCard = ({ item }) => {
             className="h-[32px] bg-[#DFF7DF] text-[#23C633] "
           />
         </div>
-        <p className="text-[16px] text-[#5F6A61]">{desc}</p>
+        <p className="text-[16px] text-[#5F6A61]">{description}</p>
       </div>
-      <Button name={`chart ${name}`} icon={chat} className="mt-[32px]" />
+      <Button name={`chart ${applicantFirstName}`} icon={chat} className="mt-[32px]" />
     </>
   );
 };
